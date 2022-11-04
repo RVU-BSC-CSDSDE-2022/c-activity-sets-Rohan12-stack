c0958acae368d77afc4a3970fdb16f90adf82f16
Rohan  B <114479383+Rohan12-stack@users.noreply.github.com>
2022-11-04 05:22:06 +0000

    problem solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -9,3 +9,13 @@ int main(void) {
   printf("Enter the second number");
   scanf("%d",&b);
   sum=sum_func(a,b);
+  printf("%d",sum);
+
+return 0;
+{
+  int sum_func(int a, int b)
+}
+int sum;
+  sum=a+b;
+  return sum;
+  }
